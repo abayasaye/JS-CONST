597534d08bcb007291855fa85908d3d4b4f85d2a
abayasaye <abay400@gmail.com>
2022-06-14 16:15:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const stringArray = []
+function stringFunc () {
+    stringArray.sort()
+    console.log(stringArray)
+}
+
+
+stringFunc (stringArray.push("zell" , "benovo" , "aop" , "moshiba"))
+
+    const numberArray = []
+function arrayFunc () {
+    numberArray.sort((a,b)=> { return a-b})
+    console.log(numberArray)
+}
+
+
+arrayFunc (numberArray.push("9", "5" , "3" , "1"))
+
+    const numbersArray = []
+ function numbersFunc () {
+    numbersArray.sort((a,b)=> b-a)
+    console.log(numbersArray);
+}
+numbersFunc (numbersArray.push("3" , "7" , "11" , "22"))
+
+
+let inputNames =document.createElement("input")
+document.body.appendChild(inputNames)
+function arrayNamesFunc () {
+
+
+}
+arrayNamesFunc ()
